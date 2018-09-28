2b11ad1e52fae32491c8d2a93754170e22dd4d25
Freeman <egor.kovalev616@gmail.com>
2018-09-28 03:54:17 +0300

    Update BuyCar.cpp

--- BuyCar.cpp
+++ BuyCar.cpp
@@ -8,18 +8,20 @@ class BuyCar
                                    double percentLossByMonth){
   std::vector<int> result(2,0);
     
-  int newPriceOld=startPriceOld, newPriceNew=startPriceNew, howMonths=0;
-  double newPercentLossByMonth=percentLossByMonth, increasePercentEveryTwoMonts = 0.5;
+  double newPriceOld=startPriceOld, 
+         newPriceNew=startPriceNew, 
+         newPercentLossByMonth=percentLossByMonth, 
+         increasePercentEveryTwoMonts = 0.5;
+  int howMonths=0;
   
   while ((newPriceOld + (savingperMonth * howMonths)) < newPriceNew){
     howMonths++;
     if (howMonths%2 == 0) newPercentLossByMonth+=increasePercentEveryTwoMonts; // Если четный месяц, то увеличиваем процент удешевления. 
-    //int bufferForPercent = 1-(newPercentLossByMonth/100); // Остаток вычитания из 100 процентов процента удешевления, переведенный в норм вид для умножения.
     newPriceOld-=(newPriceOld/100)*newPercentLossByMonth;
     newPriceNew-=(newPriceNew/100)*newPercentLossByMonth;
     };
   
-  int savingMoney= static_cast<int> ((newPriceOld + (savingperMonth * howMonths)) - newPriceNew);
+  int savingMoney = static_cast<int> ((newPriceOld + (savingperMonth * howMonths)) - newPriceNew);
   result[0] = howMonths;
   result[1] = savingMoney;
   
