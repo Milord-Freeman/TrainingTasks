2df4182be4d6f81b89343f87276da567292d29b4
Freeman <egor.kovalev616@gmail.com>
2018-09-28 03:34:24 +0300

    Update BuyCar.cpp

--- BuyCar.cpp
+++ BuyCar.cpp
@@ -13,14 +13,15 @@ class BuyCar
   
   while ((newPriceOld + (savingperMonth * howMonths)) < newPriceNew){
     howMonths++;
-    if (!howMonths%2) newPercentLossByMonth+=increasePercentEveryTwoMonts; // Если четный месяц, то увеличиваем процент удешевления. 
-    int bufferForPercent = 1-(newPercentLossByMonth/100); // Остаток вычитания из 100 процентов процента удешевления, переведенный в норм вид для умножения.
-    newPriceOld*=bufferForPercent;
-    newPriceNew*=bufferForPercent;
+    if (howMonths%2 == 0) newPercentLossByMonth+=increasePercentEveryTwoMonts; // Если четный месяц, то увеличиваем процент удешевления. 
+    //int bufferForPercent = 1-(newPercentLossByMonth/100); // Остаток вычитания из 100 процентов процента удешевления, переведенный в норм вид для умножения.
+    newPriceOld-=(newPriceOld/100)*newPercentLossByMonth;
+    newPriceNew-=(newPriceNew/100)*newPercentLossByMonth;
     };
   
+  int savingMoney= static_cast<int> ((newPriceOld + (savingperMonth * howMonths)) - newPriceNew);
   result[0] = howMonths;
-  result[1] = static_cast<int> (newPriceOld + (savingperMonth * howMonths)) - newPriceNew;
+  result[1] = savingMoney;
   
   return result;
   }
