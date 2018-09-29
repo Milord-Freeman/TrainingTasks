a4875a149419f7ac9fdeac6cdd057d75c6e5d555
Freeman <egor.kovalev616@gmail.com>
2018-09-30 02:49:19 +0300

    Create ArmstongNumber.cpp

---
+++ ArmstongNumber.cpp
@@ -0,0 +1,25 @@
+//https://www.codewars.com/kata/5287e858c6b5a9678200083c/cpp/
+
+int powerTo(int base, int power){
+  if (power>1) return (base * powerTo(base, power-1)); else if (power==0) return 1; else return base;
+}
+
+bool narcissistic( const int value ){
+  int sumOfPoweredDigits = 0;
+  int buffer = value;
+  int power = 0;
+  std::vector<int> digits;
+  
+  while (buffer){
+    power++;
+    digits.push_back(buffer%10);
+    buffer/=10;
+  }
+  
+  for (auto digit : digits){
+    sumOfPoweredDigits += powerTo(digit, power);
+  }
+  
+  if (sumOfPoweredDigits == value) return true;
+  else return false;
+}
