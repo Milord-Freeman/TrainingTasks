ac7949faf3136f124eb9da71e250c703180cfd8f
Freeman <egor.kovalev616@gmail.com>
2018-10-02 00:13:15 +0300

    Create num-roman.cpp

---
+++ num-roman.cpp
@@ -0,0 +1,32 @@
+//https://www.codewars.com/kata/roman-numerals-encoder/train/cpp
+
+#include <map>
+#include <string>
+
+using namespace std;
+
+string solution(int number){
+  map <int, string> decToRoman = {{1, "I"},
+                                  {4, "IV"},
+                                  {5, "V"},
+                                  {9, "IX"},
+                                  {10, "X"},
+                                  {40, "XL"},
+                                  {50, "L"},
+                                  {90, "XC"},
+                                  {100, "C"},
+                                  {400, "CD"},
+                                  {500, "D"},
+                                  {900, "CM"},
+                                  {1000, "M"}};
+  string result = "";
+
+  for (auto num = decToRoman.end(); (num != decToRoman.begin()) and (number >0); num--){
+    while (number > (*num).first){
+      number-=(*num).first;
+      result+=(*num).second;
+    }
+  }
+  
+  return result;
+}
