be4d0dd8aec525854d9d205c7506a0a40580b20a
Freeman <egor.kovalev616@gmail.com>
2018-10-02 20:53:14 +0300

    Create squareIntoSquares.cpp

---
+++ squareIntoSquares.cpp
@@ -0,0 +1,24 @@
+//https://www.codewars.com/kata/square-into-squares-protect-trees/train/cpp
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+class Decomp
+{
+  public:
+  static std::vector<long long> decompose(long long n);
+};
+
+Decomp::decompose(long long n){
+  long long squareOfN = pow(n, 2);
+  vector<int> result;
+  while (squareOfN){
+    for (long i = n; i > 0; i--){
+      
+  
+}
+
+testing(50, {1,3,5,8,49});
+        testing(44, {2,3,5,7,43});
+        testing(2, {});
