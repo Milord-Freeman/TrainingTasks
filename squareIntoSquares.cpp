411d969e2124215832f84fe4c1e12768d114ce44
Freeman <egor.kovalev616@gmail.com>
2018-10-06 09:45:02 +0300

    Update squareIntoSquares.cpp

--- squareIntoSquares.cpp
+++ squareIntoSquares.cpp
@@ -15,7 +15,7 @@ class Decomp
 vector<long long> Decomp::decompose(long long number){
   vector<long long> resultVector;
   long long squaredNumber = pow(number, 2);
-  while (resultVector.empty() and number>1) {
+  while (resultVector.empty() and number>0) {
     number--;
     resultVector = recDecompose(squaredNumber - pow(number, 2), number);
   }
@@ -24,23 +24,29 @@ vector<long long> Decomp::decompose(long long number){
 
 vector<long long> Decomp::recDecompose(long long difference, long long prevSquare){
   vector<long long> result;
-  if (isSquare(difference)){
-  	result.push_back(sqrt(difference));
-  	return result;
+  
+  if (isSquare(difference)){ 
+    result.push_back(sqrt(difference));
+    result.push_back(prevSquare);
+    return result;
+  } else {
+    for (long long nextSquare = prevSquare - 1; nextSquare > 1; nextSquare--){
+      if ((difference - pow(nextSquare, 2)) >= 0) {
+        result = recDecompose(difference - pow(nextSquare, 2), nextSquare);
+        if (!result.empty()) {
+          result.push_back(prevSquare);
+          return result;
+        }
+      }
+    }
   }
-  else {
-  	for (long long i = prevSquare - 1; i > 1; i--){
-  		if (difference - pow(i, 2) > 0) {
-  			result = recDecompose(difference - pow(i, 2), i);
-  			if (!result.empty()){
-  				result.push_back(i);
-  				return result;
-  			}
-  		}
-  	}
-  }
-  result.clear();
   return result; 
+}
+
+bool Decomp::isSquare(long long number){
+  if (pow(trunc(sqrt(number)), 2) == number) return true; else return false;
+}
+
 }
 
 bool Decomp::isSquare(long long number){
