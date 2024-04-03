44a6c7032f0b788171233f15350dccd580928c0a
Freeman <egor.kovalev616@gmail.com>
2024-04-03 13:44:17 +0300

    Create merge-strings-alternately.cpp

---
+++ Leetcode/merge-strings-alternately.cpp
@@ -0,0 +1,23 @@
+
+// https://leetcode.com/problems/merge-strings-alternately
+
+class Solution {
+public:
+    string mergeAlternately(const string& word1, const string& word2) {
+        const int word1Length = word1.length();
+        const int word2Length = word2.length();
+        const int maxLenght = std::max(word1Length, word2Length);
+        std::string result;
+        result.reserve(2 * maxLenght);
+
+        for (auto i = 0; i < maxLenght; i++) {
+            if (i < word1Length)
+                result.push_back(word1.at(i));
+
+            if (i < word2Length)
+                result.push_back(word2.at(i));
+        }
+
+        return result;
+    };
+};
